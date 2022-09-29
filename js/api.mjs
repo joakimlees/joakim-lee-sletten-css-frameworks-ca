ffe45c9fafc98bb8335b99d92bd12928c92fec0d
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-09-29 13:05:59 +0200

    added files and api_base_url

---
+++ js/api.mjs
@@ -0,0 +1,3 @@
+const API_BASE_URL = "https://nf-api.onrender.com/api/v1";
+
+export { API_BASE_URL };
--- js/ui.js
+++ js/ui.js
@@ -1,25 +1,25 @@
 //login functionality
 
 const loginHeading = document.getElementById("login-heading");
-const makeAccountBtn = document.getElementById("make-account-btn");
+const makeAccountBtn = document.getElementById("switch-form-btn");
 const loginForm = document.getElementById("login-form");
 const createAccountHeading = document.getElementById("create-account-heading");
 
 const createAccountContent = `
 <div class="mt-5">
   <label for="username-login" class="form-label text-white">Username</label>
-  <input type="text" class="form-control" id="username-login" aria-describedby="username" required />
+  <input type="text" class="form-control" id="username-register" aria-describedby="username" required />
 </div>
 <div class="mt-2">
   <label for="email-login" class="form-label text-white">Email address</label>
-  <input type="email" class="form-control" id="email-login" aria-describedby="email-address" required />
+  <input type="email" class="form-control" id="email-register" aria-describedby="email-address" required />
 </div>
 <div class="mb-3 mt-2">
   <label for="exampleInputPassword1" class="form-label text-white">Password</label>
-  <input type="password" class="form-control" id="exampleInputPassword1" required />
+  <input type="password" class="form-control" id="password-register" required />
 </div>
 <div class="d-grid mx-auto">
-  <button type="submit" class="btn btn-primary">Create Account and login</button>
+  <button type="submit" class="btn btn-primary" id="create-account-btn">Create Account and login</button>
 </div>
 
 `;
@@ -31,7 +31,7 @@ const loginContent = `
 </div>
 <div class="mb-3">
   <label for="exampleInputPassword1" class="form-label text-white">Password</label>
-  <input type="password" class="form-control" id="exampleInputPassword1" required />
+  <input type="password" class="form-control" id="password-login" required />
 </div>
 <div class="d-grid mx-auto">
   <button type="submit" class="btn btn-primary">Login</button>
