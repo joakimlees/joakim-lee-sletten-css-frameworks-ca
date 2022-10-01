816656d4dc60b218264933c6e9c0b7a48d0f222a
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-10-01 15:11:53 +0200

    html structure fix links

--- js/api/constants.mjs
+++ js/api/constants.mjs
@@ -1,3 +1,4 @@
-const API_BASE_URL = "https://nf-api.onrender.com/api/v1";
-
-export { API_BASE_URL };
+export const API_HOST_URL = "https://nf-api.onrender.com";
+export const API_BASE = "/api/v1";
+export const API_SOCIAL_BASE = "/social";
+export const API_SOCIAL_URL = `${API_HOST_URL}${API_BASE}${API_SOCIAL_URL}`;
