4a2c923525f0977d6d3334cfaf043ac364e76288
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-10-03 07:53:08 +0200

    made changes

--- js/api/auth/register.mjs
+++ js/api/auth/register.mjs
@@ -6,5 +6,12 @@ const method = "post";
 export async function register(profile) {
   const registerURL = API_SOCIAL_URL + action;
 
+  fetch(registerURL, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+    body,
+  });
   console.log(registerURL);
 }
